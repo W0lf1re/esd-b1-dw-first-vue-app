d69baed157316442a957c641c35aaee55af3b7e7
Yvann <yvann.nadal@mail-esd.com>
2022-06-02 11:02:10 +0200

    first commit

---
+++ src/views/ContactView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import FormContact from "@/components/FormContact.vue";
+</script>
+
+<template>
+  <div>
+    <h1>Contact view page</h1>
+
+    <FormContact />
+  </div>
+</template>
